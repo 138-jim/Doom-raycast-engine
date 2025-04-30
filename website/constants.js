ed334f09885a0cde44b7f1bd3d23bdf65eee3e97
jim <jim@hutsix.com.au>
2025-04-30 14:20:14 +0930

    maybe fixed the loading screen error again

--- website/constants.js
+++ website/constants.js
@@ -8,19 +8,7 @@ const TILE_SIZE = 64;
 const PLAYER_SIZE = 10;
 const RAY_COUNT = Math.floor(SCREEN_WIDTH / 4); // Number of rays to cast (performance adjusted)
 const WALL_HEIGHT = 50;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-const WALL_STRIP_WIDTH = Math.max(1, Math.ceil(SCREEN_WIDTH / RAY_COUNT)); // Ensure no gaps between strips and minimum width of 1px
-=======
-const WALL_STRIP_WIDTH = Math.ceil(4); // Ensure no gaps between strips
->>>>>>> Stashed changes
-=======
-const WALL_STRIP_WIDTH = Math.ceil(4); // Ensure no gaps between strips
->>>>>>> Stashed changes
-=======
 const WALL_STRIP_WIDTH = Math.ceil(SCREEN_WIDTH / RAY_COUNT); // Ensure no gaps between strips
->>>>>>> parent of 7cbf476 (updated LOD effect and fixed antialiasing errors)
 
 // Weapon settings
 const WEAPON_IDLE_POS = 0;
